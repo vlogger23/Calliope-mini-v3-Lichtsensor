395297ae78a9816ca3681996280f5d42b1a9c7f9
vlogger23 <100538071+vlogger23@users.noreply.github.com>
2025-02-18 22:37:35 +0100

    lichtsensor

--- main.ts
+++ main.ts
@@ -1,7 +1,6 @@
 basic.forever(function () {
     if (input.lightLevel() > 100) {
-        basic.showIcon(IconNames.Square)
-        basic.showIcon(IconNames.SmallSquare)
+        basic.showIcon(IconNames.No)
         music.playMelody("C5 B A G F E D C ", 120)
         basic.pause(500)
         basic.clearScreen()
