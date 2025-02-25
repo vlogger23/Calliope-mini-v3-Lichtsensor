b284c5528e443b80af63b3abaeb6062a8c84ea1a
vlogger23 <100538071+vlogger23@users.noreply.github.com>
2025-02-25 10:23:07 +0100

    update licht

--- main.ts
+++ main.ts
@@ -2,10 +2,14 @@ basic.forever(function () {
     if (input.lightLevel() > 100) {
         basic.showIcon(IconNames.No)
         music.playMelody("C5 B A G F E D C ", 120)
-        basic.pause(500)
         basic.clearScreen()
-        basic.pause(500)
     } else {
         basic.clearScreen()
+    }
+})
+basic.forever(function () {
+    if (input.lightLevel() == 0) {
+        basic.clearScreen()
+        music.stopMelody(MelodyStopOptions.All)
     }
 })
